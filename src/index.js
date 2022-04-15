c58dc0f66e08d32386623c2f3dc8fceed7858fe7
Kyaw Thu <evanch98@gmail.com>
2022-04-16 06:12:31 +0800

    Initial implementation of tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import {home} from './home';
+import {menu} from './menu';
 
+const div = document.querySelector('#content');
 home();
 
-const btn = document.querySelector('#tab1');
-btn.addEventListener('click', () => {
-    alert('OK');
+const hm = document.querySelector('#tab1');
+hm.addEventListener('click', () => {
+    div.removeChild(div.firstChild);
+    home();
+    alert('Home');
+});
+
+const mn = document.querySelector('#tab2');
+mn.addEventListener('click', () => {
+    div.removeChild(div.firstChild);
+    menu();
+    alert('Menu');
 });
