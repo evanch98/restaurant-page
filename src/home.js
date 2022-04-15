de9897b8ad01cf7a0e5c9c82238f95d109575e4e
Kyaw Thu <evanch98@gmail.com>
2022-04-15 20:58:52 +0800

    Initial implementation of home tab

--- src/home.js
+++ src/home.js
@@ -1,8 +1,18 @@
 import './style.css';
 
 function home() {
-    const div = document.querySelector('#content');
-    
+    const body = document.querySelector('body');
+    const div = document.createElement('div');
+    div.setAttribute('id', 'content');
+    div.textContent = "Hello, World!";
+
+    const home = document.createElement('div');
+    home.textContent = 'Home';
+    home.setAttribute('id', 'tab1');
+    home.style.backgroundColor = '#30475E';
+
+    div.appendChild(home);
+    body.appendChild(div);
 }
 
 export {home};
