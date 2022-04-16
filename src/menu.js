1a31b2d9e09deaea55e9e911ecd89e8a0ddc2672
Kyaw Thu <evanch98@gmail.com>
2022-04-16 19:13:26 +0800

    Finalizing the layout of menu tab

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
 import './style.css';
+import Nan from './nan.jpg';
+import Mhg from './mhg.jpg';
+import Shrimp from './shrimp.jpg';
+import Tea from './tealeaf.jpg';
 
 function menu() {
 
@@ -6,7 +10,7 @@ function menu() {
     const div = document.querySelector('div');
     
     const main = document.createElement('div');
-    main.setAttribute('id', 'main');
+    main.setAttribute('id', 'menuMain');
 
     // Home tab, Menu tab, Contact Us tab
     const home = document.createElement('div');
@@ -31,13 +35,55 @@ function menu() {
     // Restaurant Name
 
     // Middle Content
-    
+    const menuBody = document.createElement('div');
+    menuBody.classList.add('menuBody');
+
+    const dish1 = document.createElement('div');
+    const dish1Text = document.createElement('p');
+    dish1Text.textContent = "Nan Gyi Thoke";
+    dish1.classList.add('menuItem');
+    const nanDish = new Image();
+    nanDish.src = Nan;
+    dish1.appendChild(nanDish);
+    dish1.appendChild(dish1Text);
+
+    const dish2 = document.createElement('div');
+    const dish2Text = document.createElement('p');
+    dish2Text.textContent = "Mohinga";
+    dish2.classList.add('menuItem')
+    const mhgDish = new Image();
+    mhgDish.src = Mhg;
+    dish2.appendChild(mhgDish);
+    dish2.appendChild(dish2Text);
+
+    const dish3 = document.createElement('div');
+    const dish3Text = document.createElement('p');
+    dish3Text.textContent = "Myanmar Shrimp Curry";
+    dish3.classList.add('menuItem');
+    const shrimpDish = new Image();
+    shrimpDish.src = Shrimp;
+    dish3.appendChild(shrimpDish);
+    dish3.appendChild(dish3Text);
+
+    const dish4 = document.createElement('div');
+    const dish4Text = document.createElement('p');
+    dish4Text.textContent = "Laphet Thoke - Tea leaf salad";
+    dish4.classList.add('menuItem');
+    const teaDish = new Image();
+    teaDish.src = Tea;
+    dish4.appendChild(teaDish);
+    dish4.appendChild(dish4Text);
     // Middle Content
 
     const bottom = document.createElement('div');
     bottom.classList.add('bottom');
     bottom.textContent = "Created by Kyaw Thu. Little Myanmar Town © 2022 | All Rights Reserved"
 
+    menuBody.appendChild(dish1);
+    menuBody.appendChild(dish2);
+    menuBody.appendChild(dish3);
+    menuBody.appendChild(dish4);
+    main.appendChild(menuBody);
     main.appendChild(name);
     main.appendChild(home);
     main.appendChild(menu);
